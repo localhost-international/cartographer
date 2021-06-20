59f9aaa0f3830eee03ac79c21ecf61c58bcc5055
Leslie Owusu-Appiah <leslie@localhost.international>
2021-06-20 20:06:47 +0100

    chore(ui): settings

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigation } from '@react-navigation/native'
 import styled from 'styled-components/native'
@@ -7,22 +7,27 @@ import { BlurView } from '@react-native-community/blur'
 import { isDarkMode } from 'src/utils/appearance'
 import { useTheme } from 'styled-components/native'
 
-import data from 'src/data/settings.json'
-
 import { AppState } from 'src/store/reducers'
+
+import { Ethereum } from 'src/protocols/ethereum'
+
 
 import IconCurrencyEthereum from 'src/assets/icons/currencies/currency-ethereum.svg'
 
 
 export default function Settings() {
 
-	const theme = useTheme()
+  const theme = useTheme()
 
   const ethereumState = useSelector((state: AppState) => state.ethereum)
 
   const screenNavigation = useNavigation()
 
   const goBack = () => screenNavigation.goBack()
+
+  const [ searchWallet, setSearchWallet] = useState('')
+
+  const wallet = new Ethereum();
 
   return (
     <>
@@ -48,7 +53,7 @@ export default function Settings() {
             {ethereumState.ethWalletHex}
           </WalletAddressHex>
           <WalletBalance>
-            <WalletCurrencyIcon width={32} height={42} fill={theme.ui.icon} /> 
+            <WalletCurrencyIcon width={32} height={42} fill={theme.ui.icon} />
             <WalletAmount>
               {ethereumState.ethWalletBalance}
             </WalletAmount>
@@ -58,8 +63,28 @@ export default function Settings() {
           <CloseButtonText>Done</CloseButtonText>
         </CloseButton>
       </Header>
-			<Body>
-			</Body>
+      <Body>
+        <SearchWallet
+          placeholder="Search by wallet address"
+          value={searchWallet}
+          onChangeText={(address: string) => {
+            setSearchWallet(address);
+          }}
+          onSubmitEditing={(event) => {
+            const search = event.nativeEvent.text
+            console.log('TODO - Search for wallet information');
+          }}
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          returnKeyType="search"
+          keyboardAppearance={isDarkMode() ? 'dark' : 'light'}
+          blurOnSubmit={true}
+          clearButtonMode="always"
+          textContentType="none" // TODO - URL and hex?
+        />
+
+      </Body>
     </>
   )
 }
@@ -132,3 +157,17 @@ const Body = styled.View`
   background-color: ${props => props.theme.ui.background};
   flex: 1;
 `
+
+
+const SearchWallet = styled.TextInput`
+  font-size: 18px;
+  text-align: left;
+  padding: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-radius: 6px;
+  background: ${props => props.theme.ui.foreground};
+  color: ${props => props.theme.addressBar.color};
+`;
