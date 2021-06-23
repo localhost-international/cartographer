14ec2d4941a23e2a1701b6730906e29175427fa8
Leslie Owusu-Appiah <leslie@localhost.international>
2021-06-23 22:02:53 +0100

    chore(tests): test snapshot

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
 import styled from 'styled-components/native'
 
-
 export default function Test() {
-
   return (
-    <>
+    <TestView>
       <Text>Testing, testing, 1, 2, 3</Text>
-    </>
+    </TestView>
   )
 }
 
-
-// const TestView = styled.View`
-//   background-color: rgba(255,0,0,.25);
-// `
-// const Text = styled.Text`
-//   color: ${props => props.theme.colors.text};
-// `
+const TestView = styled.View`
+  background-color: rgba(255, 0, 0, 0.25);
+`
+const Text = styled.Text`
+  color: red;
+`
